0ed9b357018f7d3f868c878f4a1f6567a1fdd0e7
sking24 <sking24@vcd-ch204-01.eastern.ewu.edu>
2016-10-06 14:17:17 -0700

    added plug in

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Test scripts.js
+console.log('OK USA!!');
+
+// Test jquery 
+// $('img).hide();
+
+$('img').imageLens({
+    imageSrc: '/img/watch-zoom.jpg',
+    lensSize: 400,
+    borderColor: 'yellow'
+})
